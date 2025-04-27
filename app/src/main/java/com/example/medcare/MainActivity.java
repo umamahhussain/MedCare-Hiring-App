08c2a439ff2a13148e31528274b4cbbe2b3c77e0
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2025-04-27 14:32:30 +0500

    splash screen + login xml

---
+++ app/src/main/java/com/example/medcare/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.medcare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView title;
+    TextView subTitle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+            finish(); // Close the SplashActivity
+        }, 4000); // 3 seconds delay
+    }
+
+    public void init(){
+        title = findViewById(R.id.title);
+        subTitle = findViewById(R.id.subtitle);
+        Animation fade = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        title.startAnimation(fade);
+        subTitle.startAnimation(fade);
+    }
+}
